2bfce1f7badcbfcfd0f1a21b0e029fa5f4063d64
biniruu <biniruu@gmail.com>
2023-01-02 20:12:19 +0900

    feat: correct url of dev env

--- src/server/apis/button.ts
+++ src/server/apis/button.ts
@@ -2,7 +2,7 @@ import Axios from '../axios'
 import type ButtonType from '../../types/button'
 
 export const getButtonStyle = async () => {
-  const result = await Axios.get<ButtonType.Data>('/data/style.json')
+  const result = await Axios.get<ButtonType.Data>('/style.json')
   return result.data
 }
 
