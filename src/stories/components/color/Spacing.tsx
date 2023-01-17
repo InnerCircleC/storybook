8a960d65d4237b3d2dc5525ce947044ac7a4bd9b
biniruu <biniruu@gmail.com>
2023-01-17 22:12:22 +0900

    feat: use global.js as value of Size type

--- src/stories/components/color/Spacing.tsx
+++ src/stories/components/color/Spacing.tsx
@@ -1,5 +1,39 @@
-// TODO: 숫자를 자동으로 증가시킬 방법이 있는지 알아볼 것
-type Size = 4 | 8 | 12 | 16 | 20 | 24 | 28 | 32 | 36 | 40 | 44 | 48 | 52 | 56 | 60 | 64
+import {
+  GlobalSpace1,
+  GlobalSpace10,
+  GlobalSpace11,
+  GlobalSpace12,
+  GlobalSpace13,
+  GlobalSpace14,
+  GlobalSpace15,
+  GlobalSpace16,
+  GlobalSpace2,
+  GlobalSpace3,
+  GlobalSpace4,
+  GlobalSpace5,
+  GlobalSpace6,
+  GlobalSpace7,
+  GlobalSpace8,
+  GlobalSpace9,
+} from '../../../../theme/js/global'
+
+type Size =
+  | typeof GlobalSpace1
+  | typeof GlobalSpace2
+  | typeof GlobalSpace3
+  | typeof GlobalSpace4
+  | typeof GlobalSpace5
+  | typeof GlobalSpace6
+  | typeof GlobalSpace7
+  | typeof GlobalSpace8
+  | typeof GlobalSpace9
+  | typeof GlobalSpace10
+  | typeof GlobalSpace11
+  | typeof GlobalSpace12
+  | typeof GlobalSpace13
+  | typeof GlobalSpace14
+  | typeof GlobalSpace15
+  | typeof GlobalSpace16
 
 type Unit = 'px'
 
