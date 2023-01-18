2ba0340dfb2d8bfcda8fe9d50190afc58638c644
biniruu <biniruu@gmail.com>
2023-01-18 22:45:15 +0900

    feat: color와 spacing 변수 모듈 복제

---
+++ src/lib/data/color/palette.ts
@@ -0,0 +1,36 @@
+import {
+  GlobalBlack,
+  GlobalInfoAccent,
+  GlobalInfoNegative,
+  GlobalInfoPositive,
+  GlobalInfoWarning,
+  GlobalWhite,
+  LightBorderBorderOpaque,
+  LightBorderBorderTransparent,
+  LightContentsContentPrimary,
+  LightContentsContentSecondary,
+  LightContentsContentTertiary,
+  LightTertiary,
+} from '../../../../theme/js/global'
+
+export const colors = {
+  foundation: {
+    primary: GlobalBlack,
+    secondary: GlobalWhite,
+    tertiary: LightTertiary,
+    accent: GlobalInfoAccent,
+    negative: GlobalInfoNegative,
+    warning: GlobalInfoWarning,
+    positive: GlobalInfoPositive,
+  },
+  contents: {
+    contentDefault: GlobalWhite,
+    contentPrimary: LightContentsContentPrimary,
+    contentSecondary: LightContentsContentSecondary,
+    contentTertiary: LightContentsContentTertiary,
+  },
+  border: {
+    borderOpaque: LightBorderBorderOpaque,
+    borderTransparent: LightBorderBorderTransparent,
+  },
+}
