60778bc4f56dc65d4739a0e10654f224e4eb0707
Zeze(강재영) <wooder2050@gmail.com>
2023-01-18 19:53:56 +0900

    imp: color, spacing 변수 내보내기 추가

---
+++ src/stories/components/color/palette.ts
@@ -0,0 +1,35 @@
+import {
+  GlobalBlack,
+  GlobalInfoAccent,
+  GlobalInfoNegative,
+  GlobalInfoPositive,
+  GlobalInfoWarning,
+  GlobalWhite,
+  LightBorderBorderOpaque,
+  LightBorderBorderTransparent,
+  LightContentsContentPrimary,
+  LightContentsContentSecondary,
+  LightContentsContentTertiary,
+  LightTertiary,
+} from '../../../../theme/js/global'
+
+export const colors = {
+  foundation: {
+    primary: GlobalBlack,
+    secondary: GlobalWhite,
+    tertiary: LightTertiary,
+    accent: GlobalInfoAccent,
+    negative: GlobalInfoNegative,
+    warning: GlobalInfoWarning,
+    positive: GlobalInfoPositive,
+  },
+  contents: {
+    contentPrimary: LightContentsContentPrimary,
+    contentSecondary: LightContentsContentSecondary,
+    contentTertiary: LightContentsContentTertiary,
+  },
+  border: {
+    borderOpaque: LightBorderBorderOpaque,
+    borderTransparent: LightBorderBorderTransparent,
+  },
+}
